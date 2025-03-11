a4522f6a85d21d0e53a3b25619df3b5dd38ba120
Andressa168 <andressazhu118@gmail.com>
2025-03-11 19:50:33 -0400

    Update
    Fixed Top Down Player

--- Assets/Kenneth/TopDownPlayerMovement.cs
+++ Assets/Kenneth/TopDownPlayerMovement.cs
@@ -1,23 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.UI;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class TopDownPlayerMovement : MonoBehaviour
 {
-    private float speed = 8f;
+    public float speed = 8f;
+    
     private Vector2 movement;
-    private Rigidbody2D rb;
-    public Animator anim;
-    public bool interactingWithScreen;
 
-    void Awake()
-    {
-        anim.SetFloat("xVelocity", Mathf.Abs(rb.velocity.x));
-        rb = GetComponent<Rigidbody2D>();
-        gameObject.SetActive(true);
-    }
+    public Rigidbody2D rb;
+
+    public Animator animator;
+
+
+    //public Animator anim;
+    //public bool interactingWithScreen;
+
+    //void Awake()
+    //{
+    //    anim.SetFloat("xVelocity", Mathf.Abs(rb.velocity.x));
+    //    rb = GetComponent<Rigidbody2D>();
+    //    gameObject.SetActive(true);
+    //}
 
     // Update is called once per frame
     void Update()
@@ -25,25 +29,36 @@ public class TopDownPlayerMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        if (interactingWithScreen)
-        {
-            rb.velocity = new Vector2(0f, 0f);
-        } else
-        {
-            rb.velocity = new Vector2(movement.x * speed, movement.y * speed);
-        }
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
 
-        if ((Input.GetAxisRaw("Horizontal") == 0f) && (Input.GetAxisRaw("Vertical") == 0f))
-        {
-            anim.SetBool("isWalking", false);
-            anim.SetFloat("LastInputX", movement.x);
-            anim.SetFloat("LastInputY", movement.x);
-        } else
-        {
+        //if (interactingWithScreen)
+        //{
+        //    rb.velocity = new Vector2(0f, 0f);
+        //}
+        //else
+        //{
+        //    rb.velocity = new Vector2(movement.x * speed, movement.y * speed);
+        //}
 
-            anim.SetBool("isWalking", true);
-            anim.SetFloat("InputX", movement.x);
-            anim.SetFloat("InputY", movement.y);
-        }
+        //if ((Input.GetAxisRaw("Horizontal") == 0f) && (Input.GetAxisRaw("Vertical") == 0f))
+        //{
+        //    anim.SetBool("isWalking", false);
+        //    anim.SetFloat("LastInputX", movement.x);
+        //    anim.SetFloat("LastInputY", movement.y);
+        //} else
+        //{
+
+        //    anim.SetBool("isWalking", true);
+        //    anim.SetFloat("InputX", movement.x);
+        //    anim.SetFloat("InputY", movement.y);
+        //}
     }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+
 }
